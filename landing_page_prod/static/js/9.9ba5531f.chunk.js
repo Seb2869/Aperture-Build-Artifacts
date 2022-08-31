524a9cb395f57def417531ed66e3c6f37094e82c
gnarlycow <gnarlycow@users.noreply.github.com>
2022-08-31 22:42:58 +0000

    deploy: Aperture-Finance/Aperture-Landing-Page@b216ceadaee5bc6bcd98b7ceb92ce9ce6dd73340

--- landing_page_prod/static/js/9.9ba5531f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpaperture-landing-page"]=this["webpackJsonpaperture-landing-page"]||[]).push([[9],{304:function(e,a,s){"use strict";s.r(a),s.d(a,"default",(function(){return t}));s(1);var c=s(0);function t(e){var a=Object.keys(e.l)[0],s=Object.values(e.l)[0];return Object(c.jsxs)("div",{className:"howClass how".concat(a),children:[Object(c.jsx)("div",{className:"item",children:a}),Object(c.jsx)("div",{className:"value",children:s})]})}}}]);
-//# sourceMappingURL=9.9ba5531f.chunk.js.map
